0461e7dc074946b04144e6c5138dc4ae9f877bf0
Jae Min Ko <jvc123456@khu.ac.kr>
2022-10-14 15:47:26 +0900

    stlye : ker_size => kernel_size 변경

--- SinGAN/models.py
+++ SinGAN/models.py
@@ -5,9 +5,9 @@ import torch.nn.functional as F
 
 
 class ConvBlock(nn.Sequential):
-    def __init__(self, in_channel, out_channel, ker_size, padd, stride):
+    def __init__(self, in_channel, out_channel, kernel_size, padd, stride):
         super(ConvBlock,self).__init__()
-        self.add_module('conv',nn.Conv2d(in_channel ,out_channel,kernel_size=ker_size,stride=stride,padding=padd)),
+        self.add_module('conv',nn.Conv2d(in_channel ,out_channel,kernel_size=kernel_size,stride=stride,padding=padd)),
         self.add_module('norm',nn.BatchNorm2d(out_channel)),
         self.add_module('LeakyRelu',nn.LeakyReLU(0.2, inplace=True))
 
@@ -24,13 +24,13 @@ class WDiscriminator(nn.Module):
         super(WDiscriminator, self).__init__()
         self.is_cuda = torch.cuda.is_available()
         N = int(opt.nfc)
-        self.head = ConvBlock(opt.nc_im,N,opt.ker_size,opt.padd_size,1)
+        self.head = ConvBlock(opt.nc_im,N,opt.kernel_size,opt.padd_size,1)
         self.body = nn.Sequential()
         for i in range(opt.num_layer-2):
             N = int(opt.nfc/pow(2,(i+1)))
-            block = ConvBlock(max(2*N,opt.min_nfc),max(N,opt.min_nfc),opt.ker_size,opt.padd_size,1)
+            block = ConvBlock(max(2*N,opt.min_nfc),max(N,opt.min_nfc),opt.kernel_size,opt.padd_size,1)
             self.body.add_module('block%d'%(i+1),block)
-        self.tail = nn.Conv2d(max(N,opt.min_nfc),1,kernel_size=opt.ker_size,stride=1,padding=opt.padd_size)
+        self.tail = nn.Conv2d(max(N,opt.min_nfc),1,kernel_size=opt.kernel_size,stride=1,padding=opt.padd_size)
 
     def forward(self,x):
         x = self.head(x)
@@ -44,14 +44,14 @@ class GeneratorConcatSkip2CleanAdd(nn.Module):
         super(GeneratorConcatSkip2CleanAdd, self).__init__()
         self.is_cuda = torch.cuda.is_available()
         N = opt.nfc
-        self.head = ConvBlock(opt.nc_im,N,opt.ker_size,opt.padd_size,1) #GenConvTransBlock(opt.nc_z,N,opt.ker_size,opt.padd_size,opt.stride)
+        self.head = ConvBlock(opt.nc_im,N,opt.kernel_size,opt.padd_size,1) #GenConvTransBlock(opt.nc_z,N,opt.ker_size,opt.padd_size,opt.stride)
         self.body = nn.Sequential()
         for i in range(opt.num_layer-2):
             N = int(opt.nfc/pow(2,(i+1)))
-            block = ConvBlock(max(2*N,opt.min_nfc),max(N,opt.min_nfc),opt.ker_size,opt.padd_size,1)
+            block = ConvBlock(max(2*N,opt.min_nfc),max(N,opt.min_nfc),opt.kernel_size,opt.padd_size,1)
             self.body.add_module('block%d'%(i+1),block)
         self.tail = nn.Sequential(
-            nn.Conv2d(max(N,opt.min_nfc),opt.nc_im,kernel_size=opt.ker_size,stride =1,padding=opt.padd_size),
+            nn.Conv2d(max(N,opt.min_nfc),opt.nc_im,kernel_size=opt.kernel_size,stride =1,padding=opt.padd_size),
             nn.Tanh()
         )
     def forward(self,x,y):
