021ec53b0e8e74764fe60a0525e1f29ebdf68cbc
Jae Min Ko <jvc123456@khu.ac.kr>
2022-11-25 13:16:04 +0900

    chore : front를 nestjs 하위 directory로 이동

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('html');
+
   await app.listen(3000);
 }
 bootstrap();
+
+// import { NestFactory } from '@nestjs/core';
+// import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+//     AppModule,
+//     {
+//       transport: Transport.TCP,
+//     },
+//   );
+//   await app.listen();
+// }
+// bootstrap();
