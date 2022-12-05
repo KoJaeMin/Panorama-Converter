cb423298d399d372dcfc194861f9886427aa3aa0
Jae Min Ko <jvc123456@khu.ac.kr>
2022-12-05 13:36:33 +0900

    chore : 불필요 package 코드상  제거

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 from api.api import api_router
 
-from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
