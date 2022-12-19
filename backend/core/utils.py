6f82bbcbd754dff853571b52e16d27dcc6160396
Jae Min Ko <jvc123456@khu.ac.kr>
2022-12-19 22:13:42 +0900

    fix : getimage 기능 추가

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,4 +1,10 @@
 def getimage(imagefile : str) -> None :
     with open(imagefile, 'wb') as image:
-        content = imagefile.read()
+        content = image.read()
         image.write(content)
+        
+# def getimage(imagefile : str, img : str) -> None:
+#     with open(imagefile, 'rb') as image:
+#         with open(img, 'wb') as result:
+#             content = image.read()
+#             result.write(content)
