f0e11b78d8f0ee6b95fb5421f491aae73a5e470d
Jae Min Ko <jvc123456@khu.ac.kr>
2022-12-28 23:23:19 +0900

    feat : 이미지를 원하는 사이즈로 Resize하는 기능 추가

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,4 +1,5 @@
 from fastapi import UploadFile
+from PIL import Image
 
 async def DownloadImage(imagefile : str, img : UploadFile) -> None :
     with open(imagefile, 'wb') as image:
@@ -7,3 +8,8 @@ async def DownloadImage(imagefile : str, img : UploadFile) -> None :
         
 def IsQueueEmpty(queue : list) -> bool:
     return len(queue) == 0
+
+def ResizeImg(img_path: str, height : int, width : int) -> None:
+    img = Image.open(img_path)
+    img_resize_lanczos = img.resize((width, height), Image.LANCZOS)
+    img_resize_lanczos.save(img_path)
