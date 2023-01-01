ed0dce9e8e561730c1bc8e58d80bf1e8c399b769
Jae Min Ko <jvc123456@khu.ac.kr>
2023-01-01 20:06:10 +0900

    style : front sumbit file 변수명 교체

--- frontend/static/js/submit.js
+++ frontend/static/js/submit.js
@@ -1,31 +1,33 @@
 async function submit(url){
-    let height = Number(document.getElementById('height').value);
-    let width = Number(document.getElementById('width').value);
-    if(!Number.isInteger(height) || !Number.isInteger(width)){
+    const DELAY_MILLISECOND = 6000;
+
+    let inputImageHeight = Number(document.getElementById('height').value);
+    let inputImageWidth = Number(document.getElementById('width').value);
+    if(!Number.isInteger(inputImageHeight) || !Number.isInteger(inputImageWidth)){
         alert("Please enter integers!")
         return;
     }
-    height = parseInt(height);
-    width = parseInt(width);
-    if(height < 0 || width < 0){
+    inputImageHeight = parseInt(inputImageHeight);
+    inputImageWidth = parseInt(inputImageWidth);
+    if(inputImageHeight < 0 || inputImageWidth < 0){
         alert("Please enter positive numbers!")
         return;
     }
-    const password = document.getElementById('password').value;
-    const username = String(Math.floor(Math.random() * Math.sqrt(height * width))).padStart(4,0);
-    const index = upload_file.name.indexOf('.');
-    const filename = upload_file.name.slice(0,index);
+    const PASSWORD = document.getElementById('password').value;
+    const USERID = String(Math.floor(Math.random() * Math.sqrt(inputImageHeight * inputImageWidth))).padStart(4,0);
+    const INDEXofDOT = upload_file.name.indexOf('.');
+    const FILENAME = upload_file.name.slice(0,INDEXofDOT);
 
-    const image = new FormData();
-    image.append(`img`,upload_file)
+    let inputImage = new FormData();
+    inputImage.append(`img`,upload_file)
     ///height : int , width : int, username : str, password : str
-    const train_uri = `${url}?username=${username}&password=${password}&height=${height}&width=${width}`;
-    const TrainResponse = await Training(train_uri,image);
-    const TrainResponseJSON = await TrainResponse.json();
+    const URIforTRAIN = `${url}?username=${USERID}&password=${PASSWORD}&height=${inputImageHeight}&width=${inputImageWidth}`;
+    const RESPONSEofTRAIN = await requestForTrain(URIforTRAIN,inputImage);
+    const TrainResponseJSON = await RESPONSEofTRAIN.json();
     
 }
 
-function download(image,name){
+function downloadImage(image,name){
     const link = document.createElement('a');
     let encodedUri = window.URL.createObjectURL(image);
     link.style = 'display : none';
@@ -36,6 +38,6 @@ function download(image,name){
     document.body.removeChild(link);
 }
 
-const Training = async (uri, img)=> await fetch(uri,{method : 'POST',body : img})
-function Checking(){}
-function GetPanorama(){}
+const requestForTrain = async (uri, img) => {await fetch(uri,{method : 'POST',body : img})};
+const requestForCheck = async () => {};
+const requestForGetPanorama = async () => {};
