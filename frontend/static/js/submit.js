94625a8696b742f031edfbb02756d68e5bb1c02a
Jae Min Ko <jvc123456@khu.ac.kr>
2023-01-01 22:47:38 +0900

    feat : polling 기능 구현

--- frontend/static/js/submit.js
+++ frontend/static/js/submit.js
@@ -15,19 +15,32 @@ async function submit(url){
     }
     const PASSWORD = document.getElementById('password').value;
     const USERID = String(Math.floor(Math.random() * Math.sqrt(inputImageHeight * inputImageWidth))).padStart(4,0);
-    const INDEXofDOT = upload_file.name.indexOf('.');
-    const FILENAME = upload_file.name.slice(0,INDEXofDOT);
+    const FILEFULLNAME = upload_file.name;
+    const INDEXofDOT = FILEFULLNAME.indexOf('.');
+    const FILENAME = FILEFULLNAME.slice(0,INDEXofDOT);
 
     let inputImage = new FormData();
     inputImage.append(`img`,upload_file)
     ///height : int , width : int, username : str, password : str
-    const URIforTRAIN = `${url}?username=${USERID}&password=${PASSWORD}&height=${inputImageHeight}&width=${inputImageWidth}`;
+    const URIforTRAIN = `${url}/training?username=${USERID}&password=${PASSWORD}&height=${inputImageHeight}&width=${inputImageWidth}`;
+    const URIforCHECK = `${url}/check?username=${USERID}&password=${PASSWORD}`;
+    const URIforDOWNLOAD = `${url}/download?filename=${FILEFULLNAME}&username=${USERID}&password=${PASSWORD}`;
     const RESPONSEofTRAIN = await requestForTrain(URIforTRAIN,inputImage);
-    const TrainResponseJSON = await RESPONSEofTRAIN.json();
-    
+
+    if(RESPONSEofTRAIN.status !== 201){
+        const RESPONSEMESSAGE = await RESPONSEofTRAIN.json();
+        alert(`${RESPONSEofTRAIN.status}\nMESSAGE : ${RESPONSEMESSAGE['message']}`);
+        return;
+    }
+    const RESPONSEofCHECK = await requestForCheck(URIforCHECK);
+    if(RESPONSEofCHECK !== 200){
+        alert('Fail to make download');
+        return;
+    }
+    await requestForGetPanorama(URIforDOWNLOAD,FILENAME);
 }
 
-function downloadImage(image,name){
+async function downloadImage(image,name){
     const link = document.createElement('a');
     let encodedUri = window.URL.createObjectURL(image);
     link.style = 'display : none';
@@ -39,5 +52,31 @@ function downloadImage(image,name){
 }
 
 const requestForTrain = async (uri, img) => await fetch(uri,{method : 'POST',body : img});
-const requestForCheck = async () => {};
-const requestForGetPanorama = async () => {};
+
+const requestForCheck = async (uri) => {
+    let count = 0;
+    let status = -1;
+    const MAXIMUM_COUNT = 100;
+    const INTERVAL_TIMESET = 60 * 1000;
+    let CheckPolling = setInterval(async ()=>{
+        const response = await fetch(uri,{method : 'GET'});
+        status = response.status;
+        if(status === 202){
+            clearInterval(CheckPolling);
+            return;
+        }
+        if(count===MAXIMUM_COUNT){
+            clearInterval(CheckPolling);
+            alert("TimeOut");
+            return;
+        }
+        count++;
+    },INTERVAL_TIMESET)
+    return Promise.resolve(status);
+};
+
+const requestForGetPanorama = async (uri, filename) => {
+    const HTTPRESPONSE = await fetch(uri);
+    const ImageBlob = await HTTPRESPONSE.blob();
+    await downloadImage(ImageBlob,filename);
+};
